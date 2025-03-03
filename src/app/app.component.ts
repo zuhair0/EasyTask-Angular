2f3ac9f66e77206de1b2963b65d9b8242467e737
HP <HP@DESKTOP-2KLFQT2>
2025-03-03 13:01:48 -0800

    added user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from "./components/user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
