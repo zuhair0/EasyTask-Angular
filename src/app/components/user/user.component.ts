e3aa73ad2ba1e2d81ac8c252972d5d5afcd4ec2d
HP <HP@DESKTOP-2KLFQT2>
2025-03-04 06:32:38 -0800

    added dummy users adn their images

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-users';
+import { CommonModule } from '@angular/common';
 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+  users = DUMMY_USERS
 
+  get imagePath(){ // get methods are used in html as property not behave like a method
+    return 'assets/users/'+this.selectedUser.avatar;
+  }
 }
