9126d27c74fe4bf189815ec6e98068217daf3b0b
HP <HP@DESKTOP-2KLFQT2>
2025-03-05 11:31:15 -0800

    two-way binding

--- src/app/components/tasks/new-task/new-task.component.ts
+++ src/app/components/tasks/new-task/new-task.component.ts
@@ -1,13 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() cancelDialog = new EventEmitter()
+  
+  // in two-way binidin it doesnt matter if we are using signals of normal property, [(ngModel)] automatically detects and bind the value
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+  // enteredTitle = signal('');
+  // enteredSummary = signal('');
+  // enteredDueDate = signal('');
 
 cancellingNewTaskCreation(){
   this.cancelDialog.emit();
