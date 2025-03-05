c7c03b1328175171911875567776354cf0201dc8
HP <HP@DESKTOP-2KLFQT2>
2025-03-05 09:37:38 -0800

    created component for adding new-task

---
+++ src/app/components/tasks/new-task/new-task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+
+}
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingNewTask = false
   tasks = [
     {
       id: 't1',
@@ -40,5 +42,8 @@ export class TasksComponent {
   }
   ontaskComplete(taskId: string){
     this.tasks=this.tasks.filter((task)=>task.id!==taskId);
+  }
+  onStartAddTask(){
+    this.isAddingNewTask = true
   }
 }
