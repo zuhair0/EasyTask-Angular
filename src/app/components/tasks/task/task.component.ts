bdbc2e7087af4e58a37b31eb8b70c52ee52612f3
HP <HP@DESKTOP-2KLFQT2>
2025-03-05 06:49:57 -0800

    styling the task component and added new task componet

---
+++ src/app/components/tasks/task/task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+
+}
